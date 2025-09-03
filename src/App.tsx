8fb1278cfbd8747fdf17c9d003fbdb61d8a457ce
DevByDipto <dipto.1900@gmail.com>
2025-09-03 09:41:13 +0600

    setup redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './redux/features/counter/counterSlice'
+import type { RootState } from './redux/store'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
+      {/* <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+    </div> */}
+     <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
     </>
   )
